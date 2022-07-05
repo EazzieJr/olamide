16c71c90fff8e24f758f94f5bd564c5d236a5fc5
Ganiyu Idris <Golamide27@gmail.com>
2022-07-05 01:36:54 +0100

    Tryna make some fonts

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <Navigation />
+
+    <div>
+      <p class="font-arial text-7xl">EXPERIENCES</p>
+      <p class="mon text-7xl">OLAMIDE</p>
+      <p class=" text-7xl hap">OO:OO AM</p>
+      <p class="font-lato text-7xl uppercase">scroll down</p>
+    </div>
   </div>
 </template>
 
@@ -9,3 +16,30 @@ export default {
   name: 'IndexPage'
 }
 </script>
+
+
+<style>
+@font-face {
+  font-family: 'GT Haptik Regular';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('~assets/fonts/GT-Haptik-Regular.woff2') format('woff2');
+}
+
+@font-face {
+  font-family: 'Moglan';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('~assets/fonts/Moglan.ttf') format('ttf');
+}
+
+.hap {
+  font-family: 'GT Haptik Regular';
+}
+
+.mon {
+  font-family: 'Moglan';
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,8 @@ module.exports = {
       fontFamily: {
         lato: ['Lato'],
         haptik: ['Gt Haptik'],
+        arial: ['Arial'],
+        monglan: ['MonglanDemo'],
       },
     },
   },
