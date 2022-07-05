a0b814052d2858188d836c6921d2aed52c022f57
Ganiyu Idris <Golamide27@gmail.com>
2022-07-05 10:19:32 +0100

    Push for test

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,26 +1,40 @@
 <template>
-  <section class="">
+  <section class="divide-y-[0.5px] divide-black dark:divide-white">
+    <div>
+      <h1>olamide adeyi</h1>
+    </div>
 
+    <div class="between">
+      <img src="svg/asterisk.svg" alt="" />
+
+      <h1>work folio</h1>
+
+      <img src="svg/asterisk.svg" alt="" />
+    </div>
   </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="postcss">
 @font-face {
-  font-family: 'MoglanDemo';
+  font-family: "MoglanDemo";
   font-style: normal;
   font-weight: 400;
   font-display: swap;
-  src: url('~assets/fonts/MoglanDemo.ttf') format('ttf');
+  src: url("~assets/fonts/MoglanDemo.ttf") format("ttf");
 }
 
 section {
-  font-family: 'MoglanDemo';
-  @apply border border-y border-black dark:border-white
+  @apply border-y-[0.5px] border-black dark:border-white;
+  img {
+    @apply w-[3.7vw];
+  }
+}
+
+h1 {
+  font-family: 'MoglanDemo', sans-serif;
 }
 </style>
