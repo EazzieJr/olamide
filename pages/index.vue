5bd1709e479223cbecceb73b7061a03fbe2a6180
Ganiyu Idris <Golamide27@gmail.com>
2022-09-20 15:23:13 +0100

    Undid all

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-5 lg:px-[1.6vw]" id="index">
+  <div class="px-5 lg:px-[1.6vw]">
     <Navigation />
 
     <Hero />
@@ -17,29 +17,14 @@
 </template>
 
 <script>
-// import SmoothScrollbar from "smooth-scrollbar";
-// import ScrollTriggerPlugin from "~/js/vendor/smooth-scrollbar/ScrollTriggerPlugin";
-// import SoftScrollPlugin from "~/js/vendor/smooth-scrollbar/SoftScrollPlugin";
 import MouseFollower from "mouse-follower";
 import { gsap } from "gsap/dist/gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 MouseFollower.registerGSAP(gsap);
-// SmoothScrollbar.use(ScrollTriggerPlugin, SoftScrollPlugin);
 
 export default {
   name: "IndexPage",
-
-  methods: {
-    initScrollbar() {
-      const view = document.querySelector("#index");
-      SmoothScrollbar.init(view, {
-        renderByPixels: true,
-        damping: 0.065,
-        // alwaysShowTracks: true,
-      });
-    },
-  },
 
   mounted() {
     const cursor = new MouseFollower({
@@ -53,9 +38,6 @@ export default {
         scrollTrigger: {
           trigger: el,
           start: "top 90%",
-          pinType: "transform",
-          // markers: true,
-
           // onEnter: () => {
           // },
         },
@@ -67,24 +49,11 @@ export default {
         // opacity: 1,
       })
     })
-
-    // Init Scrollbar
-    // this.initScrollbar();
   },
 };
 </script>
 
 <style>
-/* html {
-  height: 100%;
-  display: none;
-} */
-
-#index {
-  width: 100%;
-  height: 100%;
-}
-  
 .hap {
   font-family: "GT Haptik Regular";
 }
